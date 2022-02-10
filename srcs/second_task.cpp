958354c6a393ce030d32278c5a988972119b4e9c
Михаил Ворожцов <MAVorozhtsov@sberbank.ru>
2022-02-10 18:57:40 +0300

    add insert zero

--- srcs/second_task.cpp
+++ srcs/second_task.cpp
@@ -6,17 +6,17 @@
 /*   By: jbenjy <jbenjy@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 16:44:02 by jbenjy            #+#    #+#             */
-/*   Updated: 2022/02/10 18:34:51 by jbenjy           ###   ########.fr       */
+/*   Updated: 2022/02/10 18:57:30 by jbenjy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lab.hpp"
 
-void    find_int_and_div(std::vector<float> *vec)
+static void	find_int_and_div(std::vector<float> *vec)
 {
-	std::vector<float>::iterator it = (*vec).begin();
+	std::vector<float>::iterator it = vec->begin();
 	
-	while (it != (*vec).end())
+	while (it != vec->end())
 	{
 		if (*it == (int)(*it))
 			*it /= 2;
@@ -24,7 +24,7 @@ void    find_int_and_div(std::vector<float> *vec)
 	}
 }
 
-void    find_right_neg(std::vector<float> vec)
+static void	find_right_neg(std::vector<float> vec)
 {
 	int pos = -1;
 	int count = 1;
@@ -39,9 +39,26 @@ void    find_right_neg(std::vector<float> vec)
 	}
 
 	if (pos == -1)
-		std::cout << "There is no negative numbers\n";
+		std::cout << "\nThere is no negative numbers\n";
 	else
-		std::cout << "Position of right negative number is " << pos << "\n";
+		std::cout << "\nPosition of right negative number is " << pos << "\n";
+}
+
+static void	insert_zero(std::vector<float> *vec)
+{
+	std::vector<float>::iterator it = vec->begin();
+	std::vector<float>::iterator it_next;
+
+	for (std::vector<float>::size_type i = 0; i < vec->size(); i++)
+		if (*it == 0)
+		{
+			it_next = it + 1;
+			vec->erase(it);
+			vec->insert(vec->begin(), 0);
+			it = it_next;
+		}
+		else
+			it++;
 }
 
 void    second_task()
@@ -55,4 +72,8 @@ void    second_task()
 	print_vector(vec);
 
 	find_right_neg(vec);
+
+	std::cout << "\nInsert zero at the begining\n";
+	insert_zero(&vec);
+	print_vector(vec);
 }
